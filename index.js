0ab242473d4e5aa9ca6aeb5f4aabe6454c01e5c2
puneesh-here <71883953+puneesh-here@users.noreply.github.com>
2020-09-25 18:05:01 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+function changewt(sel){
+var sel =document.querySelector("body");
+sel.style.backgroundColor="#bfbfbf" ;
+sel.style.color="#222f3e";
+}
+function changeblk(){var sel =document.querySelector("body");
+sel.style.backgroundColor="#222f3e" ;
+sel.style.color="#bfbfbf";
+}
+alert ("🥺Please see the dark theme🥺.")
